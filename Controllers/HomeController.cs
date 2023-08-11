d4f0fdb068857a3cc30a61e1980f4c6ba67fc59c
kuanshih <kuanshih2023@gmail.com>
2023-08-11 17:47:55 +0800

    0811ajax課程
    Jquery語法
    API

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -41,7 +41,40 @@ namespace prjSite.Controllers
         {
             return View();
         }
+        public IActionResult History()
+        {
+            return View();
+        }
+        public IActionResult jQuery()
+        {
+            return View();
+        }
 
+        //使用partial view 就不會有layout的版面，可以在其他view中呼叫使用@HTML.Partial("Action名稱")
+
+        public IActionResult partial1()
+        {
+            return PartialView();
+        }
+        //這邊的重點在於，如果是在其他view中呼叫使用@HTML.Partial("Action名稱")，這樣只會有 partialview中的畫面，不會通過action，因此資料是不會傳過去的
+        //而如果使用ajax的話就可以解決這個問題，所以如果要使用partialview包含資料的話，那就要使用ajax，不然就要在用viewcomponet比較麻煩
+        public IActionResult partial2()
+        {
+            ViewBag.message = "來自Action的內容";
+            return PartialView();
+        }
+
+        //basic 套件
+        public IActionResult dataTables()
+        {
+            return View();
+        }
+
+        //套API
+        public IActionResult dataTablesAPI()
+        {
+            return View();
+        }
         public IActionResult Privacy()
         {
             return View();
